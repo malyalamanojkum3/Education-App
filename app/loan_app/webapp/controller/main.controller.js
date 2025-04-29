4cad7316349ca2bf427be807198412d968a4d926
malyala manoj kumar <malyalamanojkumar@gmail.com>
2025-04-29 11:01:01 +0000

    changed main.controller

--- app/loan_app/webapp/controller/main.controller.js
+++ app/loan_app/webapp/controller/main.controller.js
@@ -4,22 +4,10 @@ sap.ui.define([
     "use strict";
 
     return Controller.extend("loanapp.controller.main", {
-<<<<<<< HEAD
-        onInit: function() {
-            // Initialization code can be added here if needed
-    
-        },
-        onLoginPress: function() {
-            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-            oRouter.navTo("LoanStatusPage");
-        },
-
-=======
         onInit() {
             var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
             oRouter.navTo("Dashboard");
         }
->>>>>>> ef39fcf415debaf8e44b2bfc6bc3b7ac7b9830d6
     });
 });
 
