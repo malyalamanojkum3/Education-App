d71d446790e71fd3746875a4dfaee6eb0806abfd
Laxith Reddy Rachamalla <laxithreddyrachamalla8802@gmail.com>
2025-04-29 12:32:17 +0000

    resolved merge conflict

--- app/loan_app/webapp/controller/loanApplication.controller.js
+++ app/loan_app/webapp/controller/loanApplication.controller.js
@@ -1,10 +1,28 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox"
+], (Controller, MessageBox) => {
     "use strict";
 
     return Controller.extend("loanapp.controller.loanApplication", {
-        onInit() {
-        }
+        onSubmit() {
+            MessageBox.success("You have applied for loan successfully\nYour loan id: 092889123");
+            
+        },
+        onCancel() {
+            sap.m.MessageToast.show("Loan application cancelled");
+            
+        },
+        
+        onChooseFile: function () {
+            var oFileUploader = document.createElement('input');
+            oFileUploader.type = 'file';
+            oFileUploader.onchange = function (event) {
+            var oFilePathInput = this.byId("filePath");
+            oFilePathInput.setValue(event.target.files[0].name);
+                 }.bind(this);
+            oFileUploader.click();
+                }
+    
     });
 });
